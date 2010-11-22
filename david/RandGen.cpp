05e446baa401931e93e590f10db7ca2e68de874f
David Björkman <david.mattias.bjorkman@gmail.com>
2010-11-22 09:26:15 +0000

    Ostkaka!

---
+++ david/RandGen.cpp
@@ -0,0 +1,134 @@
+
+#include "RandGen.h"
+
+//-------------------------------------------------------------------------------------
+RandGen::RandGen(void)
+{
+	//map arena;
+	//arena = new map;
+}
+//-------------------------------------------------------------------------------------
+RandGen::~RandGen(void)
+{
+	//delete map;
+}
+
+//-------------------------------------------------------------------------------------
+void RandGen::generateMap(int dim_x, int dim_y, int room_cnt, int room_min_x, int room_min_y, int door_cnt, bool furniture_enable, int desk_cnt, int chair_cnt, int shelf_cnt, int painting_cnt)
+{
+    //Fill structures
+	
+	//Dimensions
+	map arena;
+	arena.dim.x = dim_x;
+	arena.dim.y = dim_y;
+	arena.room_cnt = room_cnt;
+
+	//doors and Furniture 
+	
+	for (int i = 0; i < arena.room_cnt; i++)
+	{
+		arena.office[i].door.cnt = door_cnt;
+		if (furniture_enable)
+		{
+				
+			arena.office[i].desk.cnt = desk_cnt;
+			arena.office[i].chair.cnt = chair_cnt;
+			arena.office[i].shelf.cnt = shelf_cnt;
+			arena.office[i].painting.cnt = painting_cnt;
+		}
+		else
+		{
+			arena.office[i].desk.cnt = 0;
+			arena.office[i].chair.cnt = 0;
+			arena.office[i].shelf.cnt = 0;
+			arena.office[i].painting.cnt = 0;
+		}
+	}
+	//Generate Walls
+	room room[MAX_ROOMS];
+	tile origo;
+	origo.x = 0;
+	origo.y = 0;
+	arena.office->orig = origo;
+	split(arena);
+}
+void RandGen::split(map arena)
+{ 
+	int wallPos;
+	int MinWalLen; 
+	tile wall;
+	tile pos;
+	//one room input
+	for(int i=1; i<arena.room_cnt; i++)
+	{
+		//two rooms
+		sortRooms(arena);
+		tile min = getSmallestRoom();
+		if(!sizeOk(min))
+		{
+			removeWall
+		}
+		//biggest one room
+		while(room<MIN)
+		{
+			int max = getBiggestRoom();
+			if(dim.x > dim.y)
+			{
+				wallPos = randInt(0,dim.x);
+				if(wallPos < MinWalLen || (dim.x - wallPos) < MinWalLen)
+				{
+					split(dim,orig);
+				}
+				else
+				{
+					wall.x = 0;
+					wall.y = dim.y;
+					pos.x = orig.x + wall.x;
+					pos.y = orig.y;
+					addWall(wall,pos);
+				}
+			}
+			else if(y > x)
+			{
+	
+			}
+			else
+			{
+	
+			}
+		}//End while loop
+	}//End for loop
+	//om inte antal rum är tillräckligt många: split else: return
+	//wile loop
+}
+int RandGen::randInt(int low, int high)
+{
+}
+void RandGen::doors(void)
+{
+}
+void RandGen::desk(void)
+{
+}
+void RandGen::chear(void)
+{
+}
+void RandGen::shelf(void)
+{
+}
+void RandGen::painting(void)
+{
+}
+void RandGen::updateBlackTiles(void)
+{
+}
+void RandGen::addBlackTile(tile pos)
+{
+}
+void RandGen::addWall(tile len, tile pos)
+{
+}
+
+
+
---
+++ david/RandGen.h
@@ -0,0 +1,59 @@
+
+#ifndef __RandGen_h_
+#define __RandGen_h_
+
+class RandGen
+{
+public:
+    RandGen(void);
+    ~RandGen(void);
+	void generateMap(int dim_x=50, int dim_y=50, int room_cnt=10, int room_min_x=4, int room_min_y=4, int door_cnt=2, bool furniture_enable=0, int desk_cnt=1, int chair_cnt=1, int shelf_cnt=1, int painting_cnt=1);
+	void updateBlackTiles(void);
+	static const int MAX_TILES = 200;
+	static const int MAX_ROOMS = 100;
+	//#define MAX_TILES 200;
+	//#define MAX_ROOMS 100;
+	struct tile
+	{
+		int x;
+		int y;
+	};
+	struct furniture
+	{
+		int cnt;
+		tile pos;
+	};
+	struct room
+	{
+		furniture door;
+		furniture desk;
+		furniture chair;
+		furniture shelf;
+		furniture painting;
+		tile dim;
+		tile orig;
+		tile blackTile[MAX_TILES];//MAXTILES];
+	};
+	struct map
+	{
+		tile dim;
+		int room_cnt;
+		int wall_cnt;
+		room office[MAX_ROOMS];
+	};
+
+private:
+	//#define MAXTILES 100;
+	//void split(int x, int y);
+	void split(map arena);
+	int randInt(int low, int high);
+	void doors(void);
+	void desk(void);
+	void chear(void);
+	void shelf(void);
+	void painting(void);
+	void addBlackTile(tile pos);
+	void addWall(tile len, tile pos);
+};
+
+#endif
