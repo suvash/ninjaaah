0d7a0bd1017201457525b5d421db6c27b1c15aa8
David Björkman <david.mattias.bjorkman@gmail.com>
2010-11-25 15:36:33 +0000

    Got my class to compile, and show random boxes at the same time!

--- david/RandGen.cpp
+++ david/RandGen.cpp
@@ -1,6 +1,6 @@
 
 #include "RandGen.h"
-//#include "math->h"
+//#include "math.h"
 #include "stdlib.h"
 #include "time.h"
 
@@ -17,90 +17,86 @@ RandGen::~RandGen(void)
 }
 
 //-------------------------------------------------------------------------------------
-//void RandGen::generateMap(struct map *arena, int dim_x, int dim_y, int room_min_x, int room_min_y, int room_max_area, int door_cnt, bool furniture_enable, int desk_cnt, int chair_cnt, int shelf_cnt, int painting_cnt)
-void RandGen::generateMap(struct map *arena)
+void RandGen::generateMap(int dim_x, int dim_y, int room_min_x, int room_min_y, int room_max_area, int door_cnt, bool furniture_enable, int desk_cnt, int chair_cnt, int shelf_cnt, int painting_cnt)
 {
     //Fill structures
 	
 	//Dimensions
-	//map arena;
-	arena->wall_cnt=0;
-	/*arena->dim->x = dim_x;	//funkar inte struct in struct???
-	arena->dim->y = dim_y;
-	arena->office[0]->dim->x  = dim_x;
-	arena->office[0]->dim->y  = dim_y;
-	arena->office[0]->orig->x = 0;
-	arena->office[0]->orig->y = 0;
-	arena->room_cnt         = 1;
-	arena->room_min_size->x  = room_min_x;
-	arena->room_min_size->y  = room_min_y;
-	arena->room_max_area	= room_max_area;*/
+	arena.dim.x = dim_x;	//funkar inte struct in struct???
+	arena.dim.y = dim_y;
+	arena.office[0].dim.x  = dim_x;
+	arena.office[0].dim.y  = dim_y;
+	arena.office[0].orig.x = 0;
+	arena.office[0].orig.y = 0;
+	arena.room_cnt         = 1;
+	arena.room_min_size.x  = room_min_x;
+	arena.room_min_size.y  = room_min_y;
+	arena.room_max_area	= room_max_area;
 	//Black tiles
 	for(int i=0; i<MAX_TILES; i++)
 	{
 		for(int j=0; j<MAX_TILES; j++)
 		{
-			tile *pos;
-			pos->x=i;
-			pos->y=j;
+			tile pos;
+			pos.x=i;
+			pos.y=j;
 			addBlackTile(pos,arena);
 		}
 	}
+		//Generate Walls
+	split(arena);
 	//doors and Furniture 
-	
-	/*for (int i = 0; i < arena->room_cnt; i++)
+	for (int i = 0; i < arena.room_cnt; i++)
 	{
-		arena->office[i]->door->cnt = door_cnt;
+		arena.office[i].door.cnt = door_cnt;
 		if (furniture_enable)
 		{
 				
-			arena->office[i]->desk->cnt = desk_cnt;
-			arena->office[i]->chair->cnt = chair_cnt;
-			arena->office[i]->shelf->cnt = shelf_cnt;
-			arena->office[i]->painting->cnt = painting_cnt;
+			arena.office[i].desk.cnt = desk_cnt;
+			arena.office[i].chair.cnt = chair_cnt;
+			arena.office[i].shelf.cnt = shelf_cnt;
+			arena.office[i].painting.cnt = painting_cnt;
 		}
 		else
 		{
-			arena->office[i]->desk->cnt = 0;
-			arena->office[i]->chair->cnt = 0;
-			arena->office[i]->shelf->cnt = 0;
-			arena->office[i]->painting->cnt = 0;
+			arena.office[i].desk.cnt = 0;
+			arena.office[i].chair.cnt = 0;
+			arena.office[i].shelf.cnt = 0;
+			arena.office[i].painting.cnt = 0;
 		}
-	}*/
-	//Generate Walls
-	split(arena);
+	}
 }
-void RandGen::split(struct map *arena)
+void RandGen::split(struct map arena)
 { 
 	room tmp_room[2];
 	int k;	//index to active room
 	int wallPos;
-	tile *wall;
-	tile *pos;
+	tile wall;
+	tile pos;
 	
 	//Generate exterior walls
-	pos->x=arena->office[0]->orig->x;
-	pos->y=arena->office[0]->orig->y;
-	wall->x=arena->office[0]->dim->x;
-	wall->y=0;
+	pos.x=arena.office[0].orig.x;
+	pos.y=arena.office[0].orig.y;
+	wall.x=arena.office[0].dim.x;
+	wall.y=0;
 	addWall(wall,pos,arena);
 	/*-------------------------------------*/
-	pos->x=arena->office[0]->orig->x;
-	pos->y=arena->office[0]->orig->y + arena->office[0]->dim->y;
-	wall->x=arena->office[0]->dim->x;
-	wall->y=0;
+	pos.x=arena.office[0].orig.x;
+	pos.y=arena.office[0].orig.y + arena.office[0].dim.y;
+	wall.x=arena.office[0].dim.x;
+	wall.y=0;
 	addWall(wall,pos,arena);
 	/*-------------------------------------*/
-	pos->x=arena->office[0]->orig->x;
-	pos->y=arena->office[0]->orig->y;
-	wall->x=0;
-	wall->y=arena->office[0]->dim->y;
+	pos.x=arena.office[0].orig.x;
+	pos.y=arena.office[0].orig.y;
+	wall.x=0;
+	wall.y=arena.office[0].dim.y;
 	addWall(wall,pos,arena);
 	/*-------------------------------------*/
-	pos->x=arena->office[0]->orig->x + arena->office[0]->dim->x;
-	pos->y=arena->office[0]->orig->y;
-	wall->x=0;
-	wall->y=arena->office[0]->dim->y;
+	pos.x=arena.office[0].orig.x + arena.office[0].dim.x;
+	pos.y=arena.office[0].orig.y;
+	wall.x=0;
+	wall.y=arena.office[0].dim.y;
 	addWall(wall,pos,arena);
 
 	//Generate interior walls
@@ -108,33 +104,33 @@ void RandGen::split(struct map *arena)
 	{
 		k = getBiggestRoom(arena);
 		//is x the longest wall?
-		if(arena->office[k]->dim->x > arena->office[k]->dim->y)
+		if(arena.office[k].dim.x > arena.office[k].dim.y)
 		{
-			wallPos = randInt(0, arena->office[k]->dim->x);
+			wallPos = randInt(0, arena.office[k].dim.x);
 			//Room too small?
-			if( !(wallPos < arena->room_min_size->x || (arena->office[k]->dim->x - wallPos) < arena->room_min_size->x) )
+			if( !(wallPos < arena.room_min_size.x || (arena.office[k].dim.x - wallPos) < arena.room_min_size.x) )
 			{
-				wall->x = 0;
-				wall->y = arena->office[k]->dim->y;
-				pos->x = arena->office[k]->orig->x + wall->x;
-				pos->y = arena->office[k]->orig->y;
+				wall.x = 0;
+				wall.y = arena.office[k].dim.y;
+				pos.x = arena.office[k].orig.x + wall.x;
+				pos.y = arena.office[k].orig.y;
 				addWall(wall,pos,arena);
-				arena->room_cnt++;
+				arena.room_cnt++;
 			}
 		}
 		//is y the longest wall?
-		else if(arena->office[k]->dim->y > arena->office[k]->dim->x)
+		else if(arena.office[k].dim.y > arena.office[k].dim.x)
 		{
-			wallPos = randInt(0, arena->office[k]->dim->y);
+			wallPos = randInt(0, arena.office[k].dim.y);
 			//Room too small?
-			if( !(wallPos < arena->room_min_size->y || (arena->office[k]->dim->y - wallPos) < arena->room_min_size->y) )
+			if( !(wallPos < arena.room_min_size.y || (arena.office[k].dim.y - wallPos) < arena.room_min_size.y) )
 			{
-				wall->y = 0;
-				wall->x = arena->office[k]->dim->y;
-				pos->y = arena->office[k]->orig->y + wall->y;
-				pos->x = arena->office[k]->orig->x;
+				wall.y = 0;
+				wall.x = arena.office[k].dim.y;
+				pos.y = arena.office[k].orig.y + wall.y;
+				pos.x = arena.office[k].orig.x;
 				addWall(wall,pos,arena);
-				arena->room_cnt++;
+				arena.room_cnt++;
 			}
 		}
 		//the walls are equal then!
@@ -142,31 +138,31 @@ void RandGen::split(struct map *arena)
 		{
 			if(randInt(0,1)==1)//Flip a coine! Does x win?
 			{
-				wallPos = randInt(0, arena->office[k]->dim->x);
+				wallPos = randInt(0, arena.office[k].dim.x);
 				//Room too small?
-				if( !(wallPos < arena->room_min_size->x || (arena->office[k]->dim->x - wallPos) < arena->room_min_size->x) )
+				if( !(wallPos < arena.room_min_size.x || (arena.office[k].dim.x - wallPos) < arena.room_min_size.x) )
 				{
-					wall->x = 0;
-					wall->y = arena->office[k]->dim->y;
-					pos->x = arena->office[k]->orig->x + wall->x;
-					pos->y = arena->office[k]->orig->y;
+					wall.x = 0;
+					wall.y = arena.office[k].dim.y;
+					pos.x = arena.office[k].orig.x + wall.x;
+					pos.y = arena.office[k].orig.y;
 					addWall(wall,pos,arena);
-					arena->room_cnt++;
+					arena.room_cnt++;
 				}
 			}
 			//y winns
 			else
 			{
-				wallPos = randInt(0, arena->office[k]->dim->y);
+				wallPos = randInt(0, arena.office[k].dim.y);
 				//Room too small?
-				if( !(wallPos < arena->room_min_size->y || (arena->office[k]->dim->y - wallPos) < arena->room_min_size->y) )
+				if( !(wallPos < arena.room_min_size.y || (arena.office[k].dim.y - wallPos) < arena.room_min_size.y) )
 				{
-					wall->y = 0;
-					wall->x = arena->office[k]->dim->y;
-					pos->y = arena->office[k]->orig->y + wall->y;
-					pos->x = arena->office[k]->orig->x;
+					wall.y = 0;
+					wall.x = arena.office[k].dim.y;
+					pos.y = arena.office[k].orig.y + wall.y;
+					pos.x = arena.office[k].orig.x;
 					addWall(wall,pos,arena);
-					arena->room_cnt++;
+					arena.room_cnt++;
 				}
 			}
 		}
@@ -182,45 +178,45 @@ void RandGen::updateBlackTiles(void)
 {
 	//From Physics
 }
-void RandGen::addBlackTile(struct tile *pos, struct map *arena)
+void RandGen::addBlackTile(struct tile pos, struct map arena)
 {
-	arena->blackTile[pos->x][pos->y] = 1;
+	arena.blackTile[pos.x][pos.y] = 1;
 }
-void RandGen::addWall(struct tile *len, struct tile *pos, struct map *arena)
+void RandGen::addWall(struct tile len, struct tile pos, struct map arena)
 {
-	tile *p;
-	if (len->x==0)
+	tile p;
+	if (len.x==0)
 	{
-		for(int i=0; i<len->y; i++)
+		for(int i=0; i<len.y; i++)
 		{
-			p->x=pos->x;
-			p->y=pos->y + i;
+			p.x=pos.x;
+			p.y=pos.y + i;
 			addBlackTile(p, arena);
 		}
 	}
 	else
 	{
-		for(int i=0; i<len->x; i++)
+		for(int i=0; i<len.x; i++)
 		{
-			p->x=pos->x + i;
-			p->y=pos->y;
+			p.x=pos.x + i;
+			p.y=pos.y;
 			addBlackTile(p, arena);
 		}
 	}
 }
-bool RandGen::sizeOk(struct map *arena, int k)
+bool RandGen::sizeOk(struct map arena, int k)
 {
 	//Is the biggest room too big?
-	if( (arena->office[k]->dim->x * arena->office[k]->dim->y) < arena->room_max_area )return true;
+	if( (arena.office[k].dim.x * arena.office[k].dim.y) < arena.room_max_area )return true;
 	else return false;
 }
-int RandGen::getBiggestRoom(struct map *arena)
+int RandGen::getBiggestRoom(struct map arena)
 {
 	int biggest_area=0;
 	int biggest_index=0;
-	for(int i =0; i<arena->room_cnt; i++)
+	for(int i =0; i<arena.room_cnt; i++)
 	{
-		if( (arena->office[i]->dim->x * arena->office[i]->dim->y) > biggest_area)
+		if( (arena.office[i].dim.x * arena.office[i].dim.y) > biggest_area)
 		{
 			biggest_index = i;
 		}
--- david/RandGen.h
+++ david/RandGen.h
@@ -21,47 +21,49 @@ public:
 	struct furniture
 	{
 		int cnt;
-		tile *pos;
+		tile pos;
 	};
 	struct room
 	{
-		furniture *door;
-		furniture *desk;
-		furniture *chair;
-		furniture *shelf;
-		furniture *painting;
-		tile *dim;
-		tile *orig;
+		furniture door;
+		furniture desk;
+		furniture chair;
+		furniture shelf;
+		furniture painting;
+		tile dim;
+		tile orig;
 		//tile blackTile[MAX_TILES][MAX_TILES];//MAXTILES];
 	};
 	struct map
 	{
-		tile *dim;
+		tile dim;
 		int room_cnt;
-		tile *room_min_size;
+		tile room_min_size;
 		int room_max_area;
 		int wall_cnt;
-		room *office[MAX_ROOMS];
+		room office[MAX_ROOMS];
 		int blackTile[MAX_TILES][MAX_TILES]; //One or zeroo: One means obstable
 	};
-	//void generateMap(struct map *arena, int dim_x=50, int dim_y=50, int room_min_x=4, int room_min_y=4, int room_max_area=25,int door_cnt=2, bool furniture_enable=0, int desk_cnt=1, int chair_cnt=1, int shelf_cnt=1, int painting_cnt=1);
+	map arena;
+	//map arena;
+	void generateMap(int dim_x=50, int dim_y=50, int room_min_x=4, int room_min_y=4, int room_max_area=25,int door_cnt=2, bool furniture_enable=0, int desk_cnt=1, int chair_cnt=1, int shelf_cnt=1, int painting_cnt=1);
 	//void generateMap(struct map *arena, int dim_x, int dim_y, int room_min_x, int room_min_y, int room_max_area,int door_cnt, bool furniture_enable, int desk_cnt, int chair_cnt, int shelf_cnt, int painting_cnt);
-	void generateMap(struct map *arena);
+	//void generateMap();
 	void updateBlackTiles(void);
 private:
 	//#define MAXTILES 100;
 	//void split(int x, int y);
-	void split(struct map *arena);
+	void split(struct map arena);
 	int randInt(int low, int high);
 	void doors(void);
 	void desk(void);
 	void chear(void);
 	void shelf(void);
 	void painting(void);
-	void addBlackTile(struct tile *pos, struct map *arena);
-	void addWall(struct tile *len, struct tile *pos, struct map *arena);
-	bool sizeOk(struct map *arena,int k);
-	int getBiggestRoom(struct map *arena);
+	void addBlackTile(struct tile pos, struct map arena);
+	void addWall(struct tile len, struct tile pos, struct map arena);
+	bool sizeOk(struct map arena,int k);
+	int getBiggestRoom(struct map arena);
 };
 
 #endif
