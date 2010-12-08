9ade8d4489bc3e60b3cd1f6c3f8d9f138d3f978a
Magnus Andersson <andersson.c.magnus@gmail.com>
2010-12-08 16:38:20 +0000

    Writing something... Deleted a return() that totally fucked up my code!

--- magnus/OgreDemo.h
+++ magnus/OgreDemo.h
@@ -10,14 +10,16 @@ public:
     OgreDemo(void);
     virtual ~OgreDemo(void);
 
- 
+	void createCamera(void);
+
 protected:
      virtual void createScene(void);
      virtual void createFrameListener(void);
      virtual bool nextLocation(void);
      virtual bool frameRenderingQueued(const Ogre::FrameEvent &evt);
- 
-     Ogre::Real mDistance;                  // The distance the object has left to travel
+	 void updateCamera(void);
+     
+		 Ogre::Real mDistance;                  // The distance the object has left to travel
      Ogre::Vector3 mDirection;              // The direction the object is moving	
 	 Ogre::Vector3 mDestination;            // The destination the object is moving towards
 	 Ogre::Real mWalkSpeed;                 // The speed at which the object is moving
@@ -28,7 +30,8 @@ protected:
  
      Ogre::Entity *mEntity;                 // The Entity we are animating
      Ogre::SceneNode *mNode;                // The SceneNode that the Entity is attached to
-
+	 Ogre::Entity *ent2;
+	 Ogre::SceneNode *node2;
  
  
  
